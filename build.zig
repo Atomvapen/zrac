a83f0812b0f21d975ce78e70384151ebfbb2c4e6
Atomvapen <164884174+Atomvapen@users.noreply.github.com>
2024-11-24 16:06:38 +0100

    Add files via upload

--- build.zig
+++ build.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
-const rl = @import("src/raylib-zig/build.zig");
+// const rl = @import("src/raylib-zig/build.zig");
+// const rg = @import("src/raylib-zig/build.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
@@ -12,9 +13,23 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    exe.root_module.addImport("raylib", b.createModule(.{
-        .root_source_file = b.path("src/raylib-zig/lib/raylib.zig"),
-    }));
+    // exe.root_module.addImport("raylib", b.createModule(.{
+    //     .root_source_file = b.path("src/raylib-zig/lib/raylib.zig"),
+    // }));
+
+    const raylib_dep = b.dependency("raylib", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raygui = raylib_dep.module("raygui"); // raygui module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
+    //
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
@@ -25,10 +40,10 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const raylib_dep = b.dependency("raylib", .{ .target = target, .optimize = optimize });
+    // const raylib_dep = b.dependency("raylib", .{ .target = target, .optimize = optimize });
 
-    exe.linkLibrary(raylib_dep.artifact("raylib"));
-    exe.linkLibC();
+    // exe.linkLibrary(raylib_dep.artifact("raylib"));
+    // exe.linkLibC();
 
     b.installArtifact(exe);
 }
